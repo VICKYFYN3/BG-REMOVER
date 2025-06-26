370531ec7bfa6fc803ce3d0be8b372d40e7122de
VICKYFYN3 <vfokezie@gmail.com>
2025-06-26 21:31:17 +0100

    Implement clerk webhooks for user management and set up user routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,60 @@
+import {Webhook} from 'svix'
+import userModel from '../models/userModel.js'
 // API CONTROLLER FUNCTION TO MANAGE CLERK USER WITH DATABASE
 //HHTP://LOCALHIOST:4000/API/USER/WEBHOOK
 
 const clerkWebhooks = async (req, res) => {
-    
+    try {
+        //create a svix instance with clerk 
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+        await whook.verify(JSON.stringify(req.body),{
+            "svix-id":req.headers['svix-id'],
+            "svix-timestamp":req.headers['svix-timestamp'],
+            "svix-signature":req.headers['svix-signature']
+        })
+
+        const {data,type} = req.body
+
+        switch (type) {
+            case "user.created":{
+                const userData = {
+                    clerkId: data.id,
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url
+                }
+
+                await userModel.create(userData)
+                res.json({success: true, message: "User created successfully"})
+                break;
+            }
+            case "user.updated":{
+                    const userData = {
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url
+                }
+                await userModel.findOneAndUpdate({clerkId: data.id}, userData)
+                res.json({success: true, message: "User updated successfully"})
+                break;
+            }
+            case "user.deleted":{
+                await userModel.findOneAndDelete({clerkId: data.id})
+                res.json({success: true, message: "User deleted successfully"})
+                break;
+            }
+            default:
+                break;
+        }
+
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false,message: error.message})
+    }
 }
+
+export {clerkWebhooks}
